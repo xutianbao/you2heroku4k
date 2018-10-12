123e3695c3897b903efe50e212432b2baab0b520
xutianbao <xutianbao@gmail.com>
2018-10-12 21:03:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ ini_set('display_errors', '0');
 //把下面的资料改成您的
 
 //网站名字
-$siteName='DouDouTube';
+$siteName='YouTube';
 
 //youtube API V3 KEY:
 
@@ -352,7 +352,7 @@ function get_data($url){
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
-		curl_setopt($ch, CURLOPT_REFERER, 'http://www.youtube.com/');
+		curl_setopt($ch, CURLOPT_REFERER, 'https://www.youtube.com/');
 		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.91 Safari/534.30");
 		$f = curl_exec($ch);
 		curl_close($ch);
